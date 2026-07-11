474388e9d58617055c04eb1f4ea43194e25222c7
techMRB <techmrbcustom@gmail.com>
2026-07-11 21:13:05 +0530

    axios client

---
+++ src/client/axiosClient.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+const BASE_URL = "http://localhost:5000/api/"
+
+
+let currentAccessToken = null;
+
+export const setAccessToken = (token) => {
+    currentAccessToken = token 
+}
+
+export const getAccessToken = () => currentAccessToken
+
+let forceLogoutCallback = () => {}
+export const registerForceLogout = (callback) => {
+    forceLogoutCallback = callback
+}
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true
+})
+
+api.interceptors.request.use((config) => {
+    if(currentAccessToken) {
+        config.headers.Authorization = `Bearer ${currentAccessToken}`
+    }
+    return config
+})
+
+
+
+
